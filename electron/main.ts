afd83a5e40c7b65480ae1bc9670bfff1c3833d40
Dmitry Marchuk <code@dmarchuk.com>
2022-05-13 23:57:05 +0300

    add store

--- electron/main.ts
+++ electron/main.ts
@@ -1,13 +1,20 @@
 import { app, BrowserWindow } from "electron";
 import { setupThemeChangedEvent } from "./main/themeChangedEvent";
 import log from "electron-log";
+import { createStore } from "./common/config";
+import { setupConfigMain } from "./main/config";
 
 let mainWindow: BrowserWindow | null;
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
-function createWindow() {
+const storePath = app.getPath("userData");
+log.info(`\n\nStore path: ${storePath}\n\n`);
+
+const store = createStore(storePath);
+
+async function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     minWidth: 900,
@@ -21,7 +28,9 @@ function createWindow() {
     }
   });
 
-  void mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  await store.write("example", "Hello world!");
+
+  await mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   mainWindow.on("closed", () => {
     mainWindow = null;
@@ -34,6 +43,7 @@ app
 .then(() => {
   if (mainWindow) {
     setupThemeChangedEvent(mainWindow);
+    setupConfigMain(mainWindow, store);
   }
   log.info("Main ready");
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { H1 } from "./components/common/typography";
+import { useAsync } from "react-use";
 
 export function App() {
+  const state = useAsync(() => window.Main.config.read("example"));
+
   return (
     <H1>
-      Hello world
+      Hello from main: {state.value}
     </H1>
   );
 }
